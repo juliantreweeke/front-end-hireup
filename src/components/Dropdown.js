0ae609b1821b7a597b0df502612657583429628e
“juliantreweeke” <juliantreweeke@gmail.com>
2017-12-16 00:34:18 +1100

    added character blurbs

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -7,12 +7,17 @@ class Dropdown extends Component {
   constructor(props) {
     super(props);
     this.state = { results : "" };
+    this.select = this.select.bind(this);
     
   }
   
   componentDidMount() {
     console.log(this.props.results);
     
+  }
+  
+  select(name){
+    console.log(name)
   }
   
   
@@ -27,9 +32,17 @@ class Dropdown extends Component {
       <div className="Dropdown-wrapper">
       {results.map((character,i) => {
             if (results.length > 0){
-              return <div className="Dropdown" key={i}>
+              if (character.description.length > 10){
+                var description = `${character.description.substring(0,120)}..`
+              }
+              
+              
+              return <div className="Dropdown" key={i} onClick={ () => {this.select(character.name) } }>
                 <img className="character-image" src={`${character.thumbnail.path}.jpg`}></img>
-                <div className="character-name">{character.name}</div>
+                <div className="character-text">
+                <div>{character.name}</div>
+                <div className="character-description">{description}</div>
+                </div>
               </div>
              }
             })}
